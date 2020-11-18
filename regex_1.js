ab37e2b9dfcf676d0ff7003cbf863fe36547f46c
gergokutu <gergo.kutu@gmail.com>
2020-11-18 13:44:54 +0100

    Find all vowels with character class

--- regex_1.js
+++ regex_1.js
@@ -44,3 +44,11 @@ console.log("Should be true >", result);
   let result = unRegex.test(exampleStr);
   console.log("Should be true >", result);
 }
+
+// character classes > e.g [abc]
+{
+  let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+  let vowelRegex = /[aeiou]/gi;
+  let result = quoteSample.match(vowelRegex);
+  console.log("Match result for all vowels:", result);
+}
