8f06580658389ab3e2d1e58daf93c5a16d1ea242
gergokutu <gergo.kutu@gmail.com>
2020-11-18 13:22:04 +0100

    Match a liter string with different possibilities

--- regex_1.js
+++ regex_1.js
@@ -2,3 +2,17 @@ let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString);
 console.log("Should be true >", result);
+
+{
+  let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+  let waldoRegex = /Waldo/;
+  let result = waldoRegex.test(waldoIsHiding);
+  console.log("Should be true >", result);
+}
+
+{
+  let petString = "James has a pet cat.";
+  let petRegex = /dog|cat|bird|fish/;
+  let result = petRegex.test(petString);
+  console.log("Should be true >", result);
+}
